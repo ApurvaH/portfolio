3f74c01b48475173a0a2393c0d945e76615e7e4a
Apurva Hajare <apurva.hajare23@gmail.com>
2020-08-15 17:46:06 -0700

    Update page

--- js/script.js
+++ js/script.js
@@ -5,11 +5,6 @@ $(window).on("load", function() {
 })
 
 $(document).ready(function() {
-	$('#slides').superslides({
-		animation: 'fade',
-		play: 5000,
-		pagination: false
-	});
 	var typed = new Typed(".typed", {
 		strings: ["Software Engineer.", "Web Developer.", "Student."],
 		typeSpeed: 70,
